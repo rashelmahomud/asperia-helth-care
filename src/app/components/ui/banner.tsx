561f8aa653b538f7d1b184c197a42e2c6881c2a1
rashelmahomud <rashelmahmudraj1998@gmail.com>
2025-03-28 23:56:20 +0600

    updated more update ode

--- src/app/components/ui/banner.tsx
+++ src/app/components/ui/banner.tsx
@@ -37,16 +37,16 @@ const Banner = () => {
       <div className="flex flex-row overflow-hidden">
         <div className="basis-1/8 ">
           <div className="bg-[url('/asperia/main-slider-shape-1.png')] h-full bg-no-repeat animate-custom-bounce duration-300">
-            <div className="lg:p-16 bg-gray-300 h-full opacity-50 z-[-1]">
+            <div className="lg:p-16 px-3 bg-gray-300 h-full opacity-50 z-[-1]">
               <FontAwesomeIcon
                 onClick={nextSlide}
                 icon={faArrowRight}
-                className="border-4 border-gray-700 text-gray-700 hover:border-gray-500 hover:text-gray-500 translate duration-300 text-white p-3 rounded-full mr-5 my-5 mt-24"
+                className="border-4 border-primary text-gray-700 hover:border-gray-500 hover:text-gray-500 translate duration-300 text-primary p-3 rounded-full mr-5 my-5 mt-24"
               />
               <FontAwesomeIcon
                 onClick={prevSlide}
                 icon={faArrowLeft}
-                className="border-4 border-gray-700 text-gray-700 hover:border-gray-500 hover:text-gray-500 translate duration-300 text-white p-3 rounded-full"
+                className="border-4 border-primary text-gray-700 hover:border-gray-500 hover:text-gray-500 translate duration-300 text-primary p-3 rounded-full"
               />
             </div>
           </div>
@@ -54,11 +54,12 @@ const Banner = () => {
         <div className="basis-1/7">
           <div className="z-1 ml-10 absolute flex gap-5 justify-center items-center lg:mt-32 mt-10">
             <div className="">
-              <div className="bg-primary my-5 lg:w-72 h-3 rounded-full z-[-1 ]"></div>
+              <div className="bg-gradient-to-r from-primary via-inconBg to-green-500 my-5 lg:w-72 h-3 rounded-full z-[-1] "></div>
+
               <h2 className="lg:text-5xl text-2xl font-semibold leading-[60px] text-gray-800">
                 For The Frist Time <br /> In Chaittagon
               </h2>
-              <h1 className="lg:text-5xl text-2xl font-semibold mb-3 my-3 text-gray-800">
+              <h1 className="lg:text-5xl text-2xl font-semibold mb-3 my-3 text-gray-800 pb-3">
                 <span className="font-bold text-primary">
                   Supper Specialities
                 </span>
